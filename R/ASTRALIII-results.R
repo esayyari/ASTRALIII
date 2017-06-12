e08627e7d0e5c62eb21554fa6496aa0aae1d5184
esayyari <esayyari@ucsd.edu>
2017-06-12 09:20:20 -0700

    gt.sh

--- R/ASTRALIII-results.R
+++ R/ASTRALIII-results.R
@@ -6,6 +6,13 @@ p<-read.csv('data/ASTRALIII/parameter.log.info',sep=" ",header=T)
 g<-read.csv('data/ASTRALIII/gtError.csv',sep=" ",header=F)
 d<-d[d$V2 == "ASTRALIII",]
 d$V2<-droplevels(d$V2)
+
+
+tr<-read.csv('data/ASTRALIII/truecompare.csv',sep=" ",header=F)
+po<-read.csv('data/ASTRALIII/polytomystat.csv',sep=" ",header=F)
+po$poly<-(po$V10)/(po$V7-3)
+
+
 g<-g[g$V6 != "-",]
 g$V6<-droplevels(g$V6)
 g$V5<-droplevels(g$V5)
@@ -22,6 +29,10 @@ g$rf<-(g$V6+g$V9)/(g$V5+g$V8)
 
 gT<-dcast(data=g[g$V3=="non",],V1+V2~.,fun.aggregate = mean,value.var = "rf")
 names(gT)[3] = "rf"
+
+poT<-dcast(data=po[po$V4 == 1000,],V1+V2+V3~.,fun.aggregate=mean, value.var="poly")
+names(poT)[4]<-"poly"
+
 # gT1000$V4<-1000
 # gT500<-dcast(data=g[g$V3=="non",],V1+V2+V3~.,fun.aggregate = function(x){mean(head(x,500))},value.var="rf")
 # names(gT500)[4] = "rf"
@@ -38,7 +49,8 @@ gT<-merge(x=d,y=gT,by.x=c("V1","V3"),by.y=c("V1","V2"))
 
 k<-merge(x=p,y=gT,by.x="Replicate",by.y="V1")
 
-k$V4<-factor(k$V4,levels=c("non","0","3","5","7","10","20","33","75"))
+k$V4<-factor(k$V4,levels=c("non","0","3","5","7","10","20","33","50","75"))
+
 
 k$meanGtErrorbin<-cut(k$rf,breaks=c(0,1/4,1/3,1/2,1),labels=c("very low (<25%)","low (<33%)","high (<50%)","very high (<100%)"))
 #k$meanGtErrorbin<-cut(k$rf,breaks=c(min(k$rf)-0.0001,quantile(k$rf)[2:5]),labels=c("very low gt err","low gt err","high gt err","very high gt err"))
